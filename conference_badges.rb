8efff335544acd7fa11cb7e65d79c726fa6f69b3
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-04 18:32:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker (firstName)
+return "Hello, my name is #{firstName}."
+end
+
+def batch_badge_creator (array)
+  badge_messages = []
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+    msg = "Hello, my name is #{name}."
+    badge_messages = msg
+  end
+    return badge_messages
+  end
